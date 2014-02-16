5144a47fc074bc837df76e5208000321c82955ed
lizardlab <lizardlab@yahoo.com>
2014-02-15 22:07:57 -0500

    added amo editor code
    AMO editors rejected the code for having performance issues, they gave me a way to do it with regex voodoo and so here is the replacement code. Thanks Mozilla!

--- src/chrome/content/xkcd-substitution.js
+++ src/chrome/content/xkcd-substitution.js
@@ -1,100 +1,78 @@
 // This was all not my code, I just edited the word replacement part
-(function() {
+// mozilla folks rejected it because performance issues, but were nice
+// and gave me replacement code to make it faster
+function xkcdMunge(doc) {
+    var substitutions = {
+        "Witnesses": "These dudes I know",
+        "witnesses": "these dudes I know",
+        "WITNESSES": "THESE DUDE I KNOW",
+        "Allegedly": "Kinda probably",
+        "allegedly": "kinda probably",
+        "ALLEGEDLY": "KINDA PROBABLY",
+        "New Study": "Tumblr Post",
+        "new Study": "Tumblr Post",
+        "New study": "Tumblr post",
+        "NEW STUDY": "TUMBLR POST",
+        "Rebuild": "Avenge",
+        "rebuild": "avenge",
+        "REBUILD": "AVENGE",
+        "Space": "Spaaace",
+        "space": "spaaace",
+        "SPACE": "SPAACE",
+        "Google Glass": "Virtual Boy",
+        "Google glass": "Virtual boy",
+        "google Glass": "virtual Boy",
+        "google glass": "virtual boy",
+        "GOOGLE GLASS": "VIRTUAL BOY",
+        "Smartphone": "Pokédex",
+        "smartphone": "pokédex",
+        "SMARTPHONE": "POKÉDEX",
+        "Electric": "Atomic",
+        "electric": "atomic",
+        "ELECTRIC": "ATOMIC",
+        "Senator": "Elf-lord",
+        "senator": "elf-lord",
+        "SENATOR": "ELF-LORD",
+        "Car": "Cat",
+        "car": "cat",
+        "CAT": "CAT",
+        "Election": "Eating contest",
+        "election": "eating contest",
+        "ELECTION": "EATING CONTEST",
+        "Congressional Leaders": "River Spirits",
+        "Congressional leaders": "River spirits",
+        "congressional Leaders": "river Spirits",
+        "congressional leaders": "river spirits",
+        "CONGRESSIONAL LEADERS": "RIVER SPIRITS",
+        "Homeland Security": "Homestar Runner",
+        "Homeland security": "Homestar runner",
+        "homeland Security": "homestar Runner",
+        "homeland security": "homestar runner",
+        "HOMELAND SECURITY": "HOMESTAR RUNNER",
+        "Could not be reached for comment": "Is guilty and everyone knows it",
+        "could not be reached for comment": "is guilty and everyone knows it",
+    };
 
-    function walk(node) 
-    {
-        // I stole this function from here:
-        // http://is.gd/mwZp7E
-    
-        var child, next;
-    
-        switch ( node.nodeType )  
-        {
-            case 1:  // Element
-            case 9:  // Document
-            case 11: // Document fragment
-                child = node.firstChild;
-                while ( child ) 
-                {
-                    next = child.nextSibling;
-                    walk(child);
-                    child = next;
-                }
-                break;
-    
-            case 3: // Text node
-                handleText(node);
-                break;
-        }
+    var xpath = "//text()[" + Object.keys(substitutions)
+                                    .map(s => 'contains(., "' + s + '")')
+                                    .join(" or ") + "]";
+
+    var pattern = RegExp("\\b(" + Object.keys(substitutions)
+                                        .join("|") + ")\\b", "g");
+
+
+    for (let node in findXPath(xpath, doc.body || doc.documentElement))
+        node.textContent = node.textContent.replace(pattern, (m0, m1) => substitutions[m1]);
+
+    function findXPath(expression, elem) {
+        let doc = elem.ownerDocument || elem;
+
+        let resolver = n => null;
+        let iterator = doc.evaluate(expression, elem, resolver,
+                                    XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
+                                    null);
+
+        for (let i = 0; i < iterator.snapshotLength; i++)
+            yield iterator.snapshotItem(i);
     }
-    
-    function handleText(textNode)
-    {
-        var v = textNode.nodeValue;
-        // This is the part I edited
-    
-        v = v.replace(/\bWitnesses\b/g, "These dudes I know");
-        v = v.replace(/\bwitnesses\b/g, "these dudes I know");
-        v = v.replace(/\bWITNESSES\b/g, "THESE DUDE I KNOW");
-        v = v.replace(/\bAllegedly\b/g, "Kinda probably");
-        v = v.replace(/\ballegedly\b/g, "kinda probably");
-        v = v.replace(/\bALLEGEDLY\b/g, "KINDA PROBABLY");
-        v = v.replace(/\bNew Study\b/g, "Tumblr Post");
-        v = v.replace(/\bnew Study\b/g, "Tumblr Post");
-        v = v.replace(/\bNew study\b/g, "Tumblr post");
-        v = v.replace(/\bNEW STUDY\b/g, "TUMBLR POST");
-        v = v.replace(/\bRebuild\b/g, "Avenge");
-        v = v.replace(/\brebuild\b/g, "avenge");
-        v = v.replace(/\bREBUILD\b/g, "AVENGE");
-        v = v.replace(/\bSpace\b/g, "Spaaace");
-        v = v.replace(/\bspace\b/g, "spaaace");
-        v = v.replace(/\bSPACE\b/g, "SPAACE");
-        v = v.replace(/\bGoogle Glass\b/g, "Virtual Boy");
-        v = v.replace(/\bGoogle glass\b/g, "Virtual boy");
-        v = v.replace(/\bgoogle Glass\b/g, "virtual Boy");
-        v = v.replace(/\bgoogle glass\b/g, "virtual boy");
-        v = v.replace(/\bGOOGLE GLASS\b/g, "VIRTUAL BOY");
-        v = v.replace(/\bSmartphone\b/g, "Pokédex");
-        v = v.replace(/\bsmartphone\b/g, "pokédex");
-        v = v.replace(/\bSMARTPHONE\b/g, "POKÉDEX");
-        v = v.replace(/\bElectric\b/g, "Atomic");
-        v = v.replace(/\belectric\b/g, "atomic");
-        v = v.replace(/\bELECTRIC\b/g, "ATOMIC");
-        v = v.replace(/\bSenator\b/g, "Elf-lord");
-        v = v.replace(/\bsenator\b/g, "elf-lord");
-        v = v.replace(/\bSENATOR\b/g, "ELF-LORD");
-        v = v.replace(/\bCar\b/g, "Cat");
-        v = v.replace(/\bcar\b/g, "cat");
-        v = v.replace(/\bCAT\b/g, "CAT");
-        v = v.replace(/\bElection\b/g, "Eating contest");
-        v = v.replace(/\belection\b/g, "eating contest");
-        v = v.replace(/\bELECTION\b/g, "EATING CONTEST");
-        v = v.replace(/\bCongressional Leaders\b/g, "River Spirits");
-        v = v.replace(/\bCongressional leaders\b/g, "River spirits");
-        v = v.replace(/\bcongressional Leaders\b/g, "river Spirits");
-        v = v.replace(/\bcongressional leaders\b/g, "river spirits");
-        v = v.replace(/\bCONGRESSIONAL LEADERS\b/g, "RIVER SPIRITS");
-        v = v.replace(/\bHomeland Security\b/g, "Homestar Runner");
-        v = v.replace(/\bHomeland security\b/g, "Homestar runner");
-        v = v.replace(/\bhomeland Security\b/g, "homestar Runner");
-        v = v.replace(/\bhomeland security\b/g, "homestar runner");
-        v = v.replace(/\bHOMELAND SECURITY\b/g, "HOMESTAR RUNNER");
-        v = v.replace(/\bCould not be reached for comment\b/g, "Is guilty and everyone knows it");
-        v = v.replace(/\bcould not be reached for comment\b/g, "is guilty and everyone knows it");
-        // End of my editions
-    
-        textNode.nodeValue = v;
-    }
-
-    function windowLoadHandler()
-    {
-    
-        window.removeEventListener('load', windowLoadHandler);
-
-        document.getElementById('appcontent').addEventListener('DOMContentLoaded', function(e) {
-            walk(e.originalTarget.body);
-        });
-    }
-
-    window.addEventListener('load', windowLoadHandler);
-}());
+}
